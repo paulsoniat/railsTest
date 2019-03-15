fbbf66e31bd65b4c66b7263d12c098f7a2968add
paulsoniat <paulmsoniat@gmail.com>
2019-03-15 12:51:12 -0500

    [crud] completes crud operations and basic functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ActiveRecord::Base
     belongs_to :category
     validates :title, :content, :category_id, presence: true
+    has_many :line_items, inverse_of: :order
+
+    def index
+        if params[:category].blank?
+          @posts = Post.all.order("created_at DESC")
+          else
+          @category_id = Category.find_by(name: params[:category]).id
+          @posts = Post.where(category_id: @category_id).order("created_at DESC")
+          end
+        end
 end
